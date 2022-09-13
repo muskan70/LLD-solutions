194058c5c4971aea868e9ada7b0fe31a9d5ba52b
muskan70 <muskan.mangla@dotpe.in>
2022-09-13 07:53:02 +0530

    updates

--- BookManager/db/mysql.go
+++ BookManager/db/mysql.go
@@ -10,18 +10,18 @@ import (
 var Client *sql.DB
 
 func Init() (*sql.DB, error) {
-	str := "root:Ayushdd@123@tcp(127.0.0.1:3306)/library"
+	str := "root:1234@123@tcp(127.0.0.1:3306)/library"
 	var err error
 	db, err := sql.Open("mysql", str)
 	if err == nil {
 		return db, err
 	}
 	if err != nil {
-		fmt.Println("Not able to connect")
+		fmt.Println("Not able to connect", err.Error())
 	}
 	pingErr := db.Ping()
 	if pingErr != nil {
-		fmt.Println("Not able to connect")
+		fmt.Println("Not able to connect", err.Error())
 		return db, pingErr
 	}
 

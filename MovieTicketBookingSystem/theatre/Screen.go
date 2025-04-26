ccf85892d8c66df3256eb1d64dfd9d698478be8d
muskan70 <muskanmangla70@gmail.com>
2025-04-26 07:39:42 +0530

    Added seat lock code

--- MovieTicketBookingSystem/theatre/Screen.go
+++ MovieTicketBookingSystem/theatre/Screen.go
@@ -7,7 +7,7 @@ var screenId atomic.Uint64
 type Screen struct {
 	Id    uint64
 	Name  string
-	Seats []Seat
+	Seats []*Seat
 }
 
 func NewScreen(name string) *Screen {
@@ -15,4 +15,12 @@ func NewScreen(name string) *Screen {
 		Id:   screenId.Add(1),
 		Name: name,
 	}
+}
+
+func (s *Screen) AddSeats(seats []*Seat) {
+	s.Seats = append(s.Seats, seats...)
+}
+
+func (s *Screen) GetSeats() []*Seat {
+	return s.Seats
 }
--- MovieTicketBookingSystem/theatre/Show.go
+++ MovieTicketBookingSystem/theatre/Show.go
@@ -1,25 +1,65 @@
 package theatre
 
 import (
+	"sync"
 	"sync/atomic"
+	"ticketBooking/constants"
 	"time"
 )
 
 var showId atomic.Uint64
 
-type Show struct {
-	Id        uint64
-	ScreenId  uint64
-	StartTime time.Time
-	Duration  int
-	MovieId   uint64
+type SeatLock struct {
+	Status int
+	Lock   sync.Mutex
 }
 
-func NewShow(sceenId, movieId uint64, showTime time.Time) *Show {
-	return &Show{
-		Id:        showId.Add(1),
-		ScreenId:  sceenId,
-		MovieId:   movieId,
-		StartTime: showTime,
+type Show struct {
+	Id             uint64
+	ScreenId       uint64
+	StartTime      time.Time
+	Duration       int
+	MovieId        uint64
+	SeatsStatusMap map[uint64]*SeatLock
+}
+
+func NewShow(screen *Screen, movieId uint64, showTime time.Time) *Show {
+	seatsMap := make(map[uint64]*SeatLock)
+	seats := screen.GetSeats()
+	for i := range seats {
+		seatsMap[seats[i].SeatNo] = &SeatLock{
+			Status: constants.SEAT_STATUS_AVAILABLE,
+			Lock:   sync.Mutex{},
+		}
 	}
+	return &Show{
+		Id:             showId.Add(1),
+		ScreenId:       screen.Id,
+		MovieId:        movieId,
+		StartTime:      showTime,
+		SeatsStatusMap: seatsMap,
+	}
+}
+
+func (s *Show) CheckSeatAvailability(seatNo uint64) bool {
+	sL, ok := s.SeatsStatusMap[seatNo]
+	return ok && sL.Status == constants.SEAT_STATUS_AVAILABLE
+}
+
+func (s *Show) UpdateSeatStatus(seatNo uint64, status int) {
+	if sL, ok := s.SeatsStatusMap[seatNo]; ok {
+		sL.Lock.Lock()
+		defer sL.Lock.Unlock()
+		sL.Status = status
+	}
+}
+
+func (s *Show) GetAvailableSeats() []uint64 {
+	var seats []uint64
+	for seatNo, seatLock := range s.SeatsStatusMap {
+		if seatLock.Status == constants.SEAT_STATUS_AVAILABLE {
+			seats = append(seats, seatNo)
+		}
+	}
+	return seats
 }
